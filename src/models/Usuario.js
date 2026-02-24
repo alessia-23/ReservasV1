5262bd0ca15a19de399dc8af5aa7f3f4834e6920
Alessia de los Ángeles Pérez Palalcios <alessia.perez@epn.edu.ec>
2026-02-23 21:54:37 -0500

    Creación del modelo Usuario

---
+++ src/models/Usuario.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const usuarioSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    apellido: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+export default mongoose.model("Usuario", usuarioSchema);
