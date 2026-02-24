ef5cf655d572b67ede487d7ac1c694b1d8836256
Alessia de los Ángeles Pérez Palalcios <alessia.perez@epn.edu.ec>
2026-02-23 22:23:58 -0500

    Creación del modelo Reserva

---
+++ src/models/Reserva.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+// Para hacer las relaciones del cliente y el vehiculo, se utiliza el tipo ObjectId y se referencia a los modelos correspondientes.
+const reservaSchema = new mongoose.Schema({
+    codigo: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    descripcion: {
+        type: String,
+        trim: true
+    },
+    cliente: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Cliente",
+        required: true
+    },
+    vehiculo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Vehiculo",
+        required: true
+    }
+}, {
+    timestamps: true,
+    versionKey: false
+});
+
+export default mongoose.model("Reserva", reservaSchema);
